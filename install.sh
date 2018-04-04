b1af6603be59351779560effe2705414ea95481e
tengxing <tengxing7452@163.com>
2018-04-04 16:25:54 +0800

    修改.bashrc路径

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cp ~/.bashrc ~/.bashrc.bak
 mkdir ~/.trash
 cp trash.sh ~/.trash
-echo "alias rm='sh ~/.trash/trash.sh'">>.bashrc
+echo "alias rm='sh ~/.trash/trash.sh'">>~/.bashrc
 source ~/.bashrc
 echo "rm保护伞安装完毕!"
