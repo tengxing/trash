02637a2af8951ddb37e3c897df696907703d8257
tengxing <tengxing7452@163.com>
2018-05-13 09:18:19 +0800

    修复路径找不到error

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp ~/.bashrc ~/.bashrc.bak
+mkdir ~/.trash
 echo "alias rm='sh /${PWD#*/}/trash.sh'">>~/.bashrc
 source ~/.bashrc
 echo "rm保护伞安装完毕!"
--- trash.sh
+++ trash.sh
@@ -1,15 +1,18 @@
 #!/bin/sh  
-  
+org_path=`pwd`
+cd
 date=`date +%F`
-base_path=${PWD#*/}
-
-path="/"$base_path"/"$date         
-echo path  
+base_path_bak=${PWD#*/}
+base_path=`pwd`"/.trash"
+path=$base_path"/"$date         
+echo $path  
 if [ ! -d $path  ]; then         
       mkdir $path                            #创建一个目录  
   fi  
     
-  str=$1                                #第一个参数  
+  str=$1    
+  echo $str
+  #第一个参数  
   first_char=$(str:0:1)  
   if [ $first_char = '-'  ];then          #第一个参数是-f或者-rf，shift到第二个参数  
         shift  
@@ -17,6 +20,6 @@ if [ ! -d $path  ]; then
       
     while [ $# != 0  ];                     #遍历所有的参数，mv到指定path  
     do  
-          mv -f $1 $path  
+          mv -f $org_path"/"$1 $path  
             shift  
         done 
